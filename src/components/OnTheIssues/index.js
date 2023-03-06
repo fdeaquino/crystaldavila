fb25d4a6100c8074ea894e78d972545ca7e65827
FDeaquino <fidel.deaquino@gmail.com>
2023-03-06 12:26:44 -0600

    dropdown menu with link to learn more about each issue

--- src/components/OnTheIssues/index.js
+++ src/components/OnTheIssues/index.js
@@ -3,7 +3,125 @@ import React from 'react';
 function OnTheIssues() {
     return (
         <>
-        <h2>On The Issues</h2>
+            <section>
+                <div className='container h-100'>
+                    <div className='row h-100 justify-content-center hero-container'>
+                        <div className='col my-auto'>
+                            <h2 className='mb-5 issues-header-text text-uppercase'>Issues</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='py-4 py-lg-5'>
+                <div className="container h-100">
+                    <div className="row h-100 justify-content-center timeline-container">
+                        <div className="col-lg-12">
+                            <h3 className="text-center">Crystal's Priorities</h3>
+                        </div>
+                    </div>
+                    <div className="row h-100 timeline-container">
+                        <div class="col-lg-6 dropdown d-flex dropdown-right my-2">
+                            <button
+                                class="btn btn-secondary dropdown-toggle"
+                                type="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                School District Transparency
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end p-4">
+                                <li><p>Unicorn asymmetrical hammock deep v yr aesthetic. Skateboard helvetica heirloom, edison bulb plaid vegan listicle chambray forage copper mug ugh poke ethical glossier pabst. Meh grailed lomo raclette vinyl enamel pin bushwick scenester. Tofu humblebrag la croix sartorial, mixtape small batch ascot godard pork belly fanny pack. Narwhal vegan raw denim letterpress offal.</p></li>
+                                <li>
+                                    <button class="dropdown-item" type="button"><a class="dropdown-item" href="#">Learn more</a></button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 dropdown d-flex dropdown-left my-2">
+                            <button
+                                class="btn btn-secondary dropdown-toggle"
+                                type="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                School Funding
+                            </button>
+                            <ul class="dropdown-menu p-4">
+                                <li><p>Unicorn asymmetrical hammock deep v yr aesthetic. Skateboard helvetica heirloom, edison bulb plaid vegan listicle chambray forage copper mug ugh poke ethical glossier pabst. Meh grailed lomo raclette vinyl enamel pin bushwick scenester. Tofu humblebrag la croix sartorial, mixtape small batch ascot godard pork belly fanny pack. Narwhal vegan raw denim letterpress offal.</p></li>
+                                <li>
+                                    <button class="dropdown-item" type="button"><a class="dropdown-item" href="#">Learn more</a></button>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-6 dropdown d-flex dropdown-right my-2">
+                            <button
+                                class="btn btn-secondary dropdown-toggle"
+                                type="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Teacher Burnout
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end p-4">
+                                <li><p>Unicorn asymmetrical hammock deep v yr aesthetic. Skateboard helvetica heirloom, edison bulb plaid vegan listicle chambray forage copper mug ugh poke ethical glossier pabst. Meh grailed lomo raclette vinyl enamel pin bushwick scenester. Tofu humblebrag la croix sartorial, mixtape small batch ascot godard pork belly fanny pack. Narwhal vegan raw denim letterpress offal.</p></li>
+                                <li>
+                                    <button class="dropdown-item" type="button"><a class="dropdown-item" href="#">Learn more</a></button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 dropdown d-flex dropdown-left my-2">
+                            <button
+                                class="btn btn-secondary dropdown-toggle"
+                                type="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                College Readiness
+                            </button>
+                            <ul class="dropdown-menu p-4">
+                                <li><p>Unicorn asymmetrical hammock deep v yr aesthetic. Skateboard helvetica heirloom, edison bulb plaid vegan listicle chambray forage copper mug ugh poke ethical glossier pabst. Meh grailed lomo raclette vinyl enamel pin bushwick scenester. Tofu humblebrag la croix sartorial, mixtape small batch ascot godard pork belly fanny pack. Narwhal vegan raw denim letterpress offal.</p></li>
+                                <li>
+                                    <button class="dropdown-item" type="button"><a class="dropdown-item" href="#">Learn more</a></button>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-6 dropdown d-flex dropdown-right my-2">
+                            <button
+                                class="btn btn-secondary dropdown-toggle"
+                                type="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Early Childhood Education
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end p-4">
+                                <li><p>Unicorn asymmetrical hammock deep v yr aesthetic. Skateboard helvetica heirloom, edison bulb plaid vegan listicle chambray forage copper mug ugh poke ethical glossier pabst. Meh grailed lomo raclette vinyl enamel pin bushwick scenester. Tofu humblebrag la croix sartorial, mixtape small batch ascot godard pork belly fanny pack. Narwhal vegan raw denim letterpress offal.</p></li>
+                                <li>
+                                    <button class="dropdown-item" type="button"><a class="dropdown-item" href="#">Learn more</a></button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 dropdown d-flex dropdown-left my-2">
+                            <button
+                                class="btn btn-secondary dropdown-toggle"
+                                type="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                School Safety
+                            </button>
+                            <ul class="dropdown-menu p-4">
+                                <li><p>Unicorn asymmetrical hammock deep v yr aesthetic. Skateboard helvetica heirloom, edison bulb plaid vegan listicle chambray forage copper mug ugh poke ethical glossier pabst. Meh grailed lomo raclette vinyl enamel pin bushwick scenester. Tofu humblebrag la croix sartorial, mixtape small batch ascot godard pork belly fanny pack. Narwhal vegan raw denim letterpress offal.</p></li>
+                                <li>
+                                    <button class="dropdown-item" type="button"><a class="dropdown-item" href="#">Learn more</a></button>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
