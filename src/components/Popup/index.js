1c02249e978c08dfe3fee8589fcc47e70c072920
FDeaquino <fidel.deaquino@gmail.com>
2023-04-16 14:30:14 -0500

    media queries for text and form within hero, media queries for popup

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -15,22 +15,22 @@ function Popup({ onClose }) {
                         </button>
                     </div>
                     <div className='col my-auto text-center'>
-                        <div className="col-10 mx-auto">
-                            <h3 className='mt-4'>Join Our Campaign!</h3>
+                        <div className="col mx-1">
+                            <h3 className='my-3 input-form-title'>Join Our Campaign!</h3>
                             <form className="contact-form d-block mx-auto" autoComplete="off">
-                                <div className="form-group">
-                                    <label className='volunteer-card-text mb-1 bold-form-label' htmlFor="name">Name</label>
-                                    <input type="text" className="form-control volunteer-card-text text-muted" id="name" placeholder="Your Name" required />
+                                <div className="form-group mx-1">
+                                    <label className='volunteer-card-text input-form-subtitle mb-1 bold-form-label' htmlFor="name">Name</label>
+                                    <input type="text" className="form-control volunteer-card-text text-muted input-form-link" id="name" placeholder="Your Name" required />
                                 </div>
-                                <div className="form-group">
-                                    <label className='volunteer-card-text mt-3 mb-1 bold-form-label' htmlFor="email">Email</label>
-                                    <input type="email" className="form-control volunteer-card-text text-muted" id="email" placeholder="Your Email" required />
+                                <div className="form-group mx-1">
+                                    <label className='volunteer-card-text input-form-subtitle mt-3 mb-1 bold-form-label' htmlFor="email">Email</label>
+                                    <input type="email" className="form-control volunteer-card-text text-muted input-form-link" id="email" placeholder="Your Email" required />
                                 </div>
-                                <div className="form-group">
-                                    <label className='volunteer-card-text mt-3 mb-1 bold-form-label' htmlFor="phone">Phone Number</label>
-                                    <input type="tel" className="form-control volunteer-card-text text-muted" id="phone" placeholder="Your Phone Number" required />
+                                <div className="form-group mx-1">
+                                    <label className='volunteer-card-text input-form-subtitle mt-3 mb-1 bold-form-label' htmlFor="phone">Phone Number</label>
+                                    <input type="tel" className="form-control volunteer-card-text text-muted input-form-link" id="phone" placeholder="Your Phone Number" required />
                                 </div>
-                                <p className='text-muted disclosure mt-3'>By submitting your cell phone number you are agreeing to receive periodic text messages from this organization. Message and data rates may apply.</p>
+                                <p className='text-muted disclosure my-2'>By submitting your cell phone number you are agreeing to receive periodic text messages from this organization. Message and data rates may apply.</p>
                                 <button className="btn btn-moving-gradient btn-moving-gradient--blue mb-4" type="submit">Submit</button>
                             </form>
                         </div>
