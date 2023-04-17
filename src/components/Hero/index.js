fef11de90a3160f7efc5564dd76d554319e18a56
FDeaquino <fidel.deaquino@gmail.com>
2023-04-17 14:38:38 -0500

    refactoring, also added new photos to Hero component in the Past Work section, added media queries to the Hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Popup from '../Popup/index.js';
+import tasbConferenceImage from '../../assets/images/cd-photo-tasb-conference.jpeg';
+import womenInPoliticsImage from '../../assets/images/cd-photo-womeninpolitics.jpeg';
+import advocacyAtCapitolImage from '../../assets/images/cd-photo-advocacyatcapitol.jpeg';
+
 
 
 function Hero() {
@@ -15,17 +19,17 @@ function Hero() {
         {
             title: 'District Level: Fostering Student Leadership in PISD',
             description: 'Supported PISD students as they presented on school safety at the TASB Governance Conference in Galveston, providing them with opportynities to engage with School Board Members and develop leadership skills.',
-            imageUrl: 'fundraising-dinner-banner.jpg'
+            imageUrl: tasbConferenceImage
         },
         {
             title: 'Local Level: Women in Politics Panel',
             description: 'Participated in a panel discussion with women in elected office to share their experiences and accomplishments. The event aimed to promote diversity, equity, and inclusion in leadership and foster a welcoming community for all. The panel provided a platform for women to share insights and inpire others to pursue leadership roles.',
-            imageUrl: 'fundraising-dinner-banner.jpg'
+            imageUrl: womenInPoliticsImage
         },
         {
             title: 'State Level: Advocated for Increasing Funding to Public Schools',
             description: 'Proposed increasing funding to improve the quality of education provided to students. Worked with educators and school administrators to identify areas where additional funding could be most effectively utilized.',
-            imageUrl: 'town-hall-banner.jpg'
+            imageUrl: advocacyAtCapitolImage
         },
     ];
 
@@ -66,7 +70,7 @@ function Hero() {
 
     return (
         <>
-            <section>
+            <section className='large-screen-mb-2'>
                 <div className='container h-100'>
                     <div className='row h-100 justify-content-center hero-container'>
                         <div className={`col my-auto photo-container ${isHorizontal ? 'cd-photo' : ''}`}>
@@ -103,15 +107,15 @@ function Hero() {
                     </div>
                 </div>
             </section>
-            <section class="py-4 py-lg-5">
-                <div class="container h-100 card custom-card-styles" >
+            <section className="py-4 py-lg-5">
+                <div className="container h-100 card custom-card-styles" >
                     <div className='row justify-content-center align-items-center stack-quote-photo'>
-                        <div class="col-6 h-100 card-body card-width">
-                            <span class="tm--quote_icon icon_quote">
-                                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" height="1em" width="1em" fill="#003468" viewBox="0 0 34.01 30.64"><defs><style>.cls-1</style></defs><title>quotes</title><path class="cls-1" d="M14.56,0C4.24,2.17,0,9.45,0,18.91V30.64H12.6V17.93H7.28c0-7.28,2.17-11.84,8.91-13.69Z"></path><path class="cls-1" d="M32.38,0C22.06,2.17,17.82,9.45,17.82,18.91V30.64h12.6V17.93H25.1c0-7.28,2.17-11.84,8.91-13.69Z"></path></svg>				</span>
-                            <h5 class="card-title text-uppercase">Join me in creating a brighter future for pasadena, by prioritizing education that prepares students for the world.</h5>
-                            <p class="card-subtitle mb-2 text-muted">Card subtitle What we are trying to do is What we are trying to do is What we are trying to do is</p>
-                            <a href="/crystaldavila/vision" class="card-link card-link-style">Learn About Crystal's Vision</a>
+                        <div className="col-6 h-100 card-body card-width">
+                            <span className="tm--quote_icon icon_quote">
+                                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" height="1em" width="1em" fill="#003468" viewBox="0 0 34.01 30.64"><defs><style>.cls-1</style></defs><title>quotes</title><path className="cls-1" d="M14.56,0C4.24,2.17,0,9.45,0,18.91V30.64H12.6V17.93H7.28c0-7.28,2.17-11.84,8.91-13.69Z"></path><path className="cls-1" d="M32.38,0C22.06,2.17,17.82,9.45,17.82,18.91V30.64h12.6V17.93H25.1c0-7.28,2.17-11.84,8.91-13.69Z"></path></svg>				</span>
+                            <h5 className="card-title text-uppercase">Join me in creating a brighter future for pasadena, by prioritizing education that prepares students for the world.</h5>
+                            <p className="card-subtitle mb-2 text-muted">Card subtitle What we are trying to do is What we are trying to do is What we are trying to do is</p>
+                            <a href="/crystaldavila/vision" className="card-link card-link-style">Learn About Crystal's Vision</a>
                         </div>
                         <div className='col-6 cd-group-photo'></div>
                     </div>
@@ -119,51 +123,54 @@ function Hero() {
 
             </section>
             <div className='blue-wrapper rectangle-section container h-100 py-4 py-lg-5'>
-                <section class="py-4 py-lg-5 ">
-                    <div class="container text-center h-100 rectangle-section">
+                <section className="py-4 py-lg-5 ">
+                    <div className="container text-center h-100 rectangle-section">
                         <h3 className='volunteer-title'>Join The Conversation</h3>
                         <p className='volunteer-content-text volunteer-card-text'>Upcoming Events</p>
                         <div className='row mt-4 h-100 justify-content-center'>
-                            <div class="col h-100 event-styles volunteer-card-background py-3 my-2">
+                            <div className="col h-100 event-styles volunteer-card-background py-3 my-2">
                                 <h4 className='mb-3'>Café con Crystal</h4>
                                 {/* kids image goes here */}
                                 <div></div>
                                 <p className='event-info mb-1'>Restaurant Name</p>
                                 <p className='event-info mb-1'>123 Main St. Pasadena, TX 77506</p>
                                 <p className='event-info'>April 4th, 2023</p>
-                                <a><button class="btn btn-moving-gradient btn-moving-gradient--blue learn-more-btn-styles"> Learn More </button></a>
+                                {/* TODO: Add working href */}
+                                <a><button className="btn btn-moving-gradient btn-moving-gradient--blue learn-more-btn-styles"> Learn More </button></a>
                             </div>
-                            <div class="col h-100 event-styles volunteer-card-background py-3 my-2">
+                            <div className="col h-100 event-styles volunteer-card-background py-3 my-2">
                                 <h4 className='mb-3'>Dia De Los Niños Celebration</h4>
                                 {/* kids image goes here */}
                                 <div></div>
                                 <p className='event-info mb-1'>Memorial Park</p>
                                 <p className='event-info mb-1'>123 Main St. Pasadena, TX 77506</p>
                                 <p className='event-info'>April 30th, 2023</p>
-                                <a><button class="btn btn-moving-gradient btn-moving-gradient--blue learn-more-btn-styles"> Learn More </button></a>
+                                {/* TODO: Add working href */}
+                                <a><button className="btn btn-moving-gradient btn-moving-gradient--blue learn-more-btn-styles"> Learn More </button></a>
                             </div>
-                            <div class="col h-100 event-styles volunteer-card-background py-3 my-2">
+                            <div className="col h-100 event-styles volunteer-card-background py-3 my-2">
                                 <h4 className='mb-3'>Cinco de Mayo Celebration</h4>
                                 {/* kids image goes here */}
                                 <div></div>
                                 <p className='event-info mb-1'>Strawberry Park</p>
                                 <p className='event-info mb-1'>123 Main St. Pasadena, TX 77506</p>
                                 <p className='event-info'>May 5th, 2023</p>
-                                <a><button class="btn btn-moving-gradient btn-moving-gradient--blue learn-more-btn-styles"> Learn More </button></a>
+                                {/* TODO: Add working href */}
+                                <a><button className="btn btn-moving-gradient btn-moving-gradient--blue learn-more-btn-styles"> Learn More </button></a>
                             </div>
                         </div>
                     </div>
                 </section>
 
-                <section class="py-4 py-lg-5 ">
-                    <div class="container text-center h-100 rectangle-section">
+                <section className="py-4 py-lg-5 ">
+                    <div className="container text-center h-100 rectangle-section">
                         <h3 className='work-title'>Past Work and Advocacy</h3>
                         <p className='work-content-text work-card-text'>Progress in Pasadena I.S.D.</p>
                         <div className="row mt-4 h-100 justify-content-center">
                             {pastworks.map((work, index) => (
                                 <div key={index} className="col-md-6 col-lg-4 py-3 ">
                                     <div className=" bg-light-gray work-styles">
-                                        <img src={work.imageUrl} alt={work.title} className="card-img-top my-2" />
+                                        <img src={work.imageUrl} alt={work.title} className="card-img-top mb-2" />
                                         <div className="card-body">
                                             <h4 className="card-title my-2">{work.title}</h4>
                                             <p className='work-card-text p-2'>{work.description}</p>
@@ -180,7 +187,7 @@ function Hero() {
                         <h3 className='mb-5 contribution-title'>Make a Contribution</h3>
                         <p className='volunteer-card-text mt-3 contribution-content-text'>Your financial support will help us reach more voters, print campaign materials, and fuel our grassroots efforts. Every dollar makes a difference in ensuring a successful campaign.</p>
                         <a href="donation-page.html">
-                            <button class="btn btn-moving-gradient btn-moving-gradient--donate mt-3 mb-4">Donate Today</button>
+                            <button className="btn btn-moving-gradient btn-moving-gradient--donate mt-3 mb-4">Donate Today</button>
                         </a>
                     </div>
                 </section>
