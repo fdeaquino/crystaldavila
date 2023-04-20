fb4366679c48a4fe9e30099531e8fce5439f691d
FDeaquino <fidel.deaquino@gmail.com>
2023-04-20 18:49:12 -0500

    working backend with Heroku, MongoDB, Mongoose, Voter Input form working and connected to backend, need to connect Contact Form to backend, removed body-parser package

--- campaign-backend/index.js
+++ campaign-backend/index.js
@@ -1,30 +1,88 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cd-campaign-backend';
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+const Schema = mongoose.Schema;
+
+const VoterInfoSchema = new Schema({
+    name: String,
+    phoneNumber: String,
+    email: String
 });
 
-app.post('/submit-voter-info', (req, res) => {
-    const { name, phoneNumber, email } = req.body;
-    // Save the voter info to a database or send to an email, etc.
-    res.status(201).send('Voter info submitted successfully.');
+const ContactFormSchema = new Schema({
+    name: String,
+    email: String,
+    message: String
 });
 
-app.post('/submit-contact-form', (req, res) => {
-    const { name, email, message } = req.body;
-    // Save the message to a database or send to an email, etc.
-    res.status(201).send('Contact form submitted successfully.');
-});
+const VoterInfo = mongoose.model('VoterInfo', VoterInfoSchema);
+const ContactForm = mongoose.model('ContactForm', ContactFormSchema);
+
+async function connectToMongoDB() {
+    try {
+        await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Connected to MongoDB');
+        startServer();
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    }
+}
+
+function startServer() {
+    app.get('/', (req, res) => {
+        res.send('Hello, World!');
+    });
+
+    app.post('/submit-voter-info', async (req, res, next) => {
+        const { name, phoneNumber, email } = req.body;
+
+        try {
+            const voterInfo = new VoterInfo({ name, phoneNumber, email });
+            await voterInfo.save();
+            console.log('Voter info saved:', voterInfo);
+            res.status(200).json({ message: 'Voter info saved' });
+        } catch (err) {
+            console.error('Error saving voter info:', err);
+            next(err);
+        }
+    });
+
+    app.post('/submit-contact-form', async (req, res, next) => {
+        const { name, email, message } = req.body;
+
+        try {
+            const contactForm = new ContactForm({ name, email, message });
+            await contactForm.save();
+            console.log('Contact form saved:', contactForm);
+            res.status(200).json({ message: 'Contact form saved' });
+        } catch (err) {
+            console.error('Error saving contact form', err);
+            next(err);
+        }
+    });
+
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something went wrong!');
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+}
+
+connectToMongoDB();
+
+
+
--- src/api.js
+++ src/api.js
@@ -1,29 +1,29 @@
 import axios from 'axios';
 
 // Example function to submit voter info
-const submitVoterInfo = async (name, phoneNumber, email) => {
-  try {
-    const response = await axios.post('http://localhost:3001/submit-voter-info', {
-      name,
-      phoneNumber,
-      email,
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error submitting voter info:', error);
-  }
+export const submitVoterInfo = async (name, phoneNumber, email) => {
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/submit-voter-info`, {
+            name,
+            phoneNumber,
+            email,
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error submitting voter info:', error);
+    }
 };
 
 // Example function to submit contact form
-const submitContactForm = async (name, email, message) => {
-  try {
-    const response = await axios.post('http://localhost:3001/submit-contact-form', {
-      name,
-      email,
-      message,
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error submitting contact form:', error);
-  }
+export const submitContactForm = async (name, email, message) => {
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/submit-contact-form`, {
+            name,
+            email,
+            message,
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error submitting contact form:', error);
+    }
 };
