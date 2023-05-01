f6a6b7063af0d14be67dc86e402724297f09d762
FDeaquino <fidel.deaquino@gmail.com>
2023-05-01 15:37:42 -0500

    added translations for Popup component

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import ConfirmationPopup from '../ConfirmationPopup';
+import { useTranslation } from 'react-i18next';
+
 
 function Popup({ onClose, formData, setFormData, handleHeroFormSubmit }) {
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const { t } = useTranslation();
+
 
     const handlePopupFormSubmit = async (e) => {
         e.preventDefault();
@@ -11,8 +15,8 @@ function Popup({ onClose, formData, setFormData, handleHeroFormSubmit }) {
     };
 
     const popupContainerClass = isSubmitted
-    ? "popup-background popup-hidden"
-    : "popup-background popup-container";
+        ? "popup-background popup-hidden"
+        : "popup-background popup-container";
 
     return (
         <>
@@ -29,41 +33,41 @@ function Popup({ onClose, formData, setFormData, handleHeroFormSubmit }) {
                     </div>
                     <div className='col my-auto text-center'>
                         <div className="col mx-1">
-                            <h3 className='my-3 input-form-title'>Join Our Campaign!</h3>
+                            <h3 className='my-3 input-form-title'>{t('join_campaign')}</h3>
                             <form className="contact-form d-block mx-auto" autoComplete="off" onSubmit={(e) => handlePopupFormSubmit(e)}>
                                 <div className="form-group mx-1">
-                                    <label className='volunteer-card-text input-form-subtitle mb-1 bold-form-label' htmlFor="name">Name</label>
+                                    <label className='volunteer-card-text input-form-subtitle mb-1 bold-form-label' htmlFor="name">{t('name')}</label>
                                     <input type="text"
                                         className="form-control volunteer-card-text text-muted input-form-link"
                                         id="name"
-                                        placeholder="Your Name"
+                                        placeholder={t('your_name')}
                                         required
                                         value={formData.name}
                                         onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
                                 </div>
                                 <div className="form-group mx-1">
-                                    <label className='volunteer-card-text input-form-subtitle mt-3 mb-1 bold-form-label' htmlFor="email">Email</label>
+                                    <label className='volunteer-card-text input-form-subtitle mt-3 mb-1 bold-form-label' htmlFor="email">{t('email')}</label>
                                     <input type="email"
                                         className="form-control volunteer-card-text text-muted input-form-link"
                                         id="email"
-                                        placeholder="Your Email"
+                                        placeholder={t('your_email')}
                                         required
                                         value={formData.email}
                                         onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
                                 </div>
                                 <div className="form-group mx-1">
-                                    <label className='volunteer-card-text input-form-subtitle mt-3 mb-1 bold-form-label' htmlFor="phone">Phone Number</label>
+                                    <label className='volunteer-card-text input-form-subtitle mt-3 mb-1 bold-form-label' htmlFor="phone">{t('phone_number')}</label>
                                     <input type="tel"
                                         className="form-control volunteer-card-text text-muted input-form-link"
                                         id="phone"
-                                        placeholder="Your Phone Number"
+                                        placeholder={t('your_phone_number')}
                                         required
                                         value={formData.phoneNumber}
                                         onChange={(e) => setFormData({ ...formData, phoneNumber: e.target.value })}
                                     />
                                 </div>
-                                <p className='text-muted disclosure my-2'>By submitting your cell phone number you are agreeing to receive periodic text messages from this organization. Message and data rates may apply.</p>
-                                <button className="btn btn-moving-gradient btn-moving-gradient--blue mb-4" type="submit">Submit</button>
+                                <p className='text-muted disclosure my-2'>{t('disclosure')}</p>
+                                <button className="btn btn-moving-gradient btn-moving-gradient--blue mb-4" type="submit">{t('submit')}</button>
                             </form>
                         </div>
                     </div>
